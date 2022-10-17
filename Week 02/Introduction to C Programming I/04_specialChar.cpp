0b001cae8e770dd6c990c63dcd99dcc705349435
Rhio Sutoyo <rhio.sutoyo@gmail.com>
2022-10-17 12:01:10 +0700

    update for week 2

---
+++ Week 02/Introduction to C Programming I/04_specialChar.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+	printf("\"Hello BINUS\"\n");
+	printf("! @ # $ %% ^ & * ( )\n");
+	printf("1 2 3 4 5 6 7                                             8 9 0\n");
+	printf("~ ` - _ + = { [ } }\n");
+	printf("| \\ : ; \" ' < , > . ? /\n");
+	printf("Special Character: %% \\ \"\n");
+	
+	// TRY TO PRINT:
+	/* Bina Nusantara University, also known as, "BINUS UNIVERSITY"
+	   is a private university in Indonesia. In BINUS, 30% of its
+	   alumnae work in global companies. Bina Nusantara \ BINUS
+	   currently has more than 100,000 alumane.
+	*/	
+	return(0);
+}
