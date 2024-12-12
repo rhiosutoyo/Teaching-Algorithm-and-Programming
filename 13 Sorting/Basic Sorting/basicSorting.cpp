0b41743cbc95b47f827dceb8bc39cb509b5e0bb0
Rhio Sutoyo <rhio.sutoyo@gmail.com>
2024-12-12 12:04:26 +0700

    add selection sort

--- 13 Sorting/Basic Sorting/basicSorting.cpp
+++ 13 Sorting/Basic Sorting/basicSorting.cpp
@@ -18,6 +18,25 @@ void bubbleSort(int data[], int n){
 	}
 }
 
+void swap(int *a, int *b){
+	int temp;
+	
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void SelectionSort(int data[], int n){
+	int i, j, idx_low;
+	
+	for(i=0;i<n-1;i++){
+		idx_low = i;
+		for (j = i + 1; j < n; j++) 
+			if (data[idx_low] < data[j]) idx_low = j;
+		if (idx_low > i) swap(&data[i], &data[idx_low]);
+    }
+}
+
 // FOR PRINT RESULT
 void printData(int data[], int n){
 	int i;
@@ -29,9 +48,13 @@ int main(){
 	int data[100] = {60,50,40,30,10,20};
 
 	printData(data, 6);
-	puts("");
 
+	puts("\n\nBubble Sort (ASCENDING)");
 	bubbleSort(data, 6);
+	printData(data, 6);	
+	
+	puts("\n\nSelection Sort (DESCENDING)");
+	SelectionSort(data, 6);
 	printData(data, 6);	
 
 	return (0);
